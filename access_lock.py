4824e0e6b7af0c0994153a55b7ca33052bfc08a6
vishalshirsat88-ai <vishal.shirsat88@gmail.com>
2026-02-20 00:11:37 +0530

    Update access_lock.py

--- access_lock.py
+++ access_lock.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import requests
 import uuid
-from info_panel import get_help_html
-
+from info_panel import show_getting_started_panel
 
 BACKEND_URL = "https://global-job-aggregator-production.up.railway.app"
 
@@ -76,10 +75,8 @@ def verify_access():
         if "welcome_shown" not in st.session_state:
             st.session_state.welcome_shown = True
         
-            # Open help popup automatically
-            st.session_state["show_help_card"] = True
-
-
+            with st.expander("⭐ Important — Please Read First", expanded=True):
+                show_getting_started_panel()
 
             #st.info("🚀 You now have lifetime access. Enjoy exploring global job opportunities!")
 
