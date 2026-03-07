e98874c4fe14579654ee2e24a7e6dddb0e206a59
vishalshirsat88-ai <vishal.shirsat88@gmail.com>
2026-03-08 03:30:13 +0530

    Update recover_payment.py

--- backend/recover_payment.py
+++ backend/recover_payment.py
@@ -1,8 +1,8 @@
 from backend.payments.db import save_payment
 from backend.services.email_service import send_access_email
 
-email = "user@email.com"
-order_id = "order_xxxxx"
+email = "michaelfernandis66@gmail.com""
+order_id = "order_SOQTtwLyZYviBS"
 
 token = save_payment(email, order_id)
 
